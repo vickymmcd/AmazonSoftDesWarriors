088f5bc43fbd5de19e47b2d0b526562b3ff07cf8
Vicky McDermott <vickymmcd@gmail.com>
2017-03-30 14:27:38 -0400

    merges

--- format_data.py
+++ format_data.py
@@ -10,10 +10,11 @@ class Formatter:
         url: url of data to obtain from Tracktor
         file_name: name of file where id of data is saved
         data_file_name: name of file where data is saved
-        NOTE: Only need data_file_name if it already exists
-        other two inputs can be empty strings
+        NOTE: Only need data_file_name and data_file if they
+        already exist, the other input can be an empty string.
         '''
         self.data_dict = get_data_dict(url, file_name, data_file_name)
+        self.poly_func = 0
 
     def add_in_between_dates(self):
         self.data_dict

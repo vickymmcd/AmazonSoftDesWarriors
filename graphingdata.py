9029206eec68a8abc83731cf3c2e6bbb216564ce
nrivkin <noah.rivkin@students.olin.edu>
2017-04-09 17:11:58 -0400

    working graphdata.py

--- graphingdata.py
+++ graphingdata.py
@@ -4,12 +4,6 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 
 formatter = Formatter("", "umbrella.txt", "umbrella_data.txt")
 frame = formatter.data_to_dataframe()
-# frame
-# plt.figure()
-# plt.plot(frame)
-# plt.show()
-
-# frame = frame.drop(frame.columns[[1]], axis = 1, inplacse = True)
 frame
 # frame2 = frame.rolling(window = 12, center = False).mean()
 decomposition = seasonal_decompose(frame, freq=365)
@@ -23,4 +17,3 @@ plt.plot(trend)
 plt.plot(seasonal)
 # plt.plot(resid)
 plt.show()
-

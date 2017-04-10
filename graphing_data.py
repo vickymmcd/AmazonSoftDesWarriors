36ac900f989b0b114832d260a6658269dce2f1e4
nrivkin <noah.rivkin@students.olin.edu>
2017-04-09 22:31:45 -0400

    Maded graphing_data.py object oriented with grapher class

--- graphing_data.py
+++ graphing_data.py
@@ -1,8 +1,27 @@
 from format_data import Formatter
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+class grapher:
+    '''
+    contains functions and classes to graph time series
+    '''
+    def __init__(self, url, file_name, data_file_name):
+        self.ts_data = Formatter(url, file_name, data_file_name)
+        self.ts.frame = self.ts_data.data_to_dataframe()
+
+    def decompose_ts(self):
+        self.ts.decomposition = seasonal_decompose(self.ts.frame, freq=365)
+        self.seasonal = self.ts.decomposition.seasonal
+        self.trend = self.ts.decomposition.trend
+        self.resid = self.ts.decomposition.resid
+
+    def graph_data(self):
+        pass
+
 # from bokeh.charts import Line, show, output_file
-formatter = Formatter("", "umbrella.txt", "umbrella_data.txt")
+formatter = Formatter("", "christmas.txt", "christmas_data.txt")
 frame = formatter.data_to_dataframe()
 decomposition = seasonal_decompose(frame, freq=365)
 seasonal = decomposition.seasonal

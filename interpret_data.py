2caad644fb7482c8d7bbdc884d762dc2f160b7cd
Katya <katya@computer.com>
2017-04-09 22:22:51 -0400

    boutta merge

--- interpret_data.py
+++ interpret_data.py
@@ -4,6 +4,7 @@ from format_data import Formatter
 import matplotlib.pyplot as plt
 import time
 import numpy as np
+from graphingdata import resid
 from sklearn import svm, linear_model
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
@@ -15,21 +16,21 @@ from sklearn.linear_model import LinearRegression
 
 
 class Interpreter:
-    def __init__(self, url, file_name, data_file_name, n_days):
-        '''
-        Initializes the data Interpreter object with
-        a data Formatter object and uses that object to grab
-        the formatted x and y values. Initializes the number of
-        days to predict into the future and generates
-        a list of wanted days.
+	def __init__(self, url, file_name, data_file_name, n_days):
+		'''
+		Initializes the data Interpreter object with
+		a data Formatter object and uses that object to grab
+		the formatted x and y values. Initializes the number of
+		days to predict into the future and generates
+		a list of wanted days.
 
-        url: url of data to obtain from Tracktor
-        file_name: name of file where id of data is saved
-        data_file_name: name of file where data is saved
-        NOTE: Only need data_file_name and data_file if those files
-        already exist, the url input can be an empty string.
-        n_days: number of days user wants the model to extrapolate
-        into the future
+		url: url of data to obtain from Tracktor
+		file_name: name of file where id of data is saved
+		data_file_name: name of file where data is saved
+		NOTE: Only need data_file_name and data_file if those files
+		already exist, the url input can be an empty string.
+		n_days: number of days user wants the model to extrapolate
+		into the future
 		'''
 		self.formatter = Formatter(url, file_name, data_file_name)
 		self.time_series = self.formatter.data_to_dataframe()
@@ -43,11 +44,6 @@ class Interpreter:
 		#print(self.ts_log_diff)
 
 	def create_acf(self):
-		#min_val = np.amin(self.ts_log_diff)
-		#print(self.ts_log_diff)
-		#print(self.ts_log_diff.columns[])
-		#print(self.ts_log_diff[[0]])
-		#print(self.ts_log_diff[:0])
 		np_to_list= []
 		for i in self.ts_log_diff.iloc[:, 0].tolist():
 			np_to_list.append(i)
@@ -59,17 +55,10 @@ class Interpreter:
 			#print(x)
 			x = x - min_val
 			x_values.append(x)
-			#print(x)
-		#print(np_to_list)
-		#print(x_values)
 		lag_acf = acf(x_values[1:],nlags=20)
 		#print(lag_acf)
 		#print(lag_acf)
 		lag_pacf = pacf(x_values[1:],nlags=20, method = 'ols')
-		#print(lag_pacf)
-		"""plt.figure()
-		plt.subplot(lag_acf, 'ro')
-		plt.show()"""
 		#for a 95% confidence interval
 		#Plot ACF:
 		plt.subplot(121)
@@ -86,72 +75,17 @@ class Interpreter:
 		plt.axhline(y=1.96/np.sqrt(len(x_values)),linestyle='--',color='gray')
 		plt.title('Partial Autocorrelation Function')
 		plt.tight_layout()
-
+		print(resid)
 		print(self.ts_log)
 		ts_log_list=[]
 		for i in self.ts_log.iloc[:, 0].tolist():
 			ts_log_list.append(i)
-		print(ts_log_list)
-		model = ARIMA(self.ts_log , order=(1,1,1))
+		model = ARIMA(resid , order=(1,1,1))
 		results_AR= model.fit()
 		plt.subplot(123)
 		plt.plot(self.ts_log_diff)
 		plt.plot(results_AR.fittedvalues, color='red')
 		plt.show()
-
-    def differencing(self):
-        ts_log = np.log(self.time_series)
-        #print(self.time_series)
-        print(ts_log.shift())
-        print(ts_log)
-        self.ts_log_diff = ts_log - ts_log.shift()
-        #print(self.ts_log_diff)
-
-    def create_acf(self):
-        #min_val = np.amin(self.ts_log_diff)
-        #print(self.ts_log_diff)
-        #print(self.ts_log_diff.columns[])
-        #print(self.ts_log_diff[[0]])
-        #print(self.ts_log_diff[:0])
-        np_to_list= []
-        for i in self.ts_log_diff.iloc[:, 0].tolist():
-        	np_to_list.append(i)
-
-        x_values =[]
-        min_val = min(np_to_list[1:])
-        print(min_val)
-        for x in np_to_list:
-        	#print(x)
-        	x = x - min_val
-        	x_values.append(x)
-        	print(x)
-        print(np_to_list)
-        print(x_values)
-        lag_acf = acf(x_values[1:],nlags=20)
-        #print(lag_acf)
-        lag_pacf = pacf(x_values[1:],nlags=20, method = 'ols')
-        #print(lag_pacf)
-        """plt.figure()
-        plt.subplot(lag_acf, 'ro')
-        plt.show()"""
-        #for a 95% confidence interval
-        #Plot ACF:
-        plt.subplot(121)
-        plt.plot(lag_acf)
-        plt.axhline(y=0,linestyle='--',color='gray')
-        plt.axhline(y=-1.96/np.sqrt(len(x_values)),linestyle='--',color='gray')
-        plt.axhline(y=1.96/np.sqrt(len(x_values)),linestyle='--',color='gray')
-        plt.title('Autocorrelation Function')
-
-        plt.subplot(122)
-        plt.plot(lag_pacf)
-        plt.axhline(y=0,linestyle='--',color='gray')
-        plt.axhline(y=-1.96/np.sqrt(len(x_values)),linestyle='--',color='gray')
-        plt.axhline(y=1.96/np.sqrt(len(x_values)),linestyle='--',color='gray')
-        plt.title('Partial Autocorrelation Function')
-        plt.tight_layout()
-        plt.show()
-
 
 
 #test_interpreter = Interpreter('', 'camera.txt', 'camera_data.txt',30)

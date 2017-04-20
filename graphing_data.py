432b460d81e543c8a9751ed60d23c97558db1306
Katya <katya@computer.com>
2017-04-19 22:31:32 -0400

    adding some changes

--- graphing_data.py
+++ graphing_data.py
@@ -1,7 +1,7 @@
 from format_data import Formatter
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
-
+import math
 
 class Grapher:
 	'''
@@ -15,8 +15,24 @@ class Grapher:
 		self.ts_decomposition = seasonal_decompose(self.ts_frame, freq=365)
 		self.seasonal = self.ts_decomposition.seasonal.dropna()
 		self.trend = self.ts_decomposition.trend.dropna()
-		self.resid = self.ts_decomposition.resid.dropna()
-		return self.resid
+		self.resid = self.ts_decomposition.resid
+		resid_list = []
+		for i in self.resid.iloc[:, 0].tolist():
+			resid_list.append(i)
+		print(resid_list)
+		for i in range(len(resid_list)):
+			if math.isnan(resid_list[i]) != True:
+				start_i = i
+				break
+		resid_list= resid_list[start_i:]
+		for i in range(len(resid_list)):
+			if math.isnan(resid_list[i]) == True:
+				end_i = i
+				break
+		print(resid_list[start_i:end_i])
+		self.resid= self.resid.dropna()
+		#print(self.resid)
+		return self.resid, start_i, end_i
 
 	def graph_data(self):
 		pass
@@ -24,10 +40,9 @@ class Grapher:
 # from bokeh.charts import Line, show, output_file
 mygrapher = Grapher("", "christmas.txt", "christmas_data.txt")
 mygrapher.decompose_ts()
-
-plt.figure()
+"""plt.figure()
 plt.plot(mygrapher.ts_frame)
 plt.plot(mygrapher.trend)
 plt.plot(mygrapher.seasonal)
 plt.plot(mygrapher.resid)
-plt.show()
+plt.show()"""

39ca666a974d893541b72dfb65710b9e97441ea8
Vicky McDermott <vickymmcd@gmail.com>
2017-05-03 17:40:33 -0400

    deleting unnecessary imports

--- interpreter_final.py
+++ interpreter_final.py
@@ -3,7 +3,6 @@ from format_data import Formatter
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from graphing_data import Grapher
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller, acf, pacf
 import statsmodels.api as sm
